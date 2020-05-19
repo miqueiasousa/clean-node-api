81561147db4f71b35eedb41c6acdc54b3f4b44b1
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-05-19 19:57:21 -0300

    move all instances to factory helper

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,10 +1,13 @@
 const LoginRouter = require('./login-router')
 const HttpRequest = require('../helpers/http-request')
 
+const makeSut = () => new LoginRouter()
+const makeHttpRequest = (req) => new HttpRequest(req)
+
 describe('Login router', () => {
   test('Sould return 400 if no email is provied', () => {
-    const sut = new LoginRouter()
-    const httpRequest = new HttpRequest({
+    const sut = makeSut()
+    const httpRequest = makeHttpRequest({
       body: {
         password: 'querty'
       }
@@ -15,8 +18,8 @@ describe('Login router', () => {
   })
 
   test('Sould return 400 if no password is provied', () => {
-    const sut = new LoginRouter()
-    const httpRequest = new HttpRequest({
+    const sut = makeSut()
+    const httpRequest = makeHttpRequest({
       body: {
         email: 'any@any.com'
       }
@@ -27,14 +30,14 @@ describe('Login router', () => {
   })
 
   test('Sould return 500 if no httpRequest is provied', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpResponse = sut.route()
 
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Sould return 500 if httpRequest has no body', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpResponse = sut.route({})
 
     expect(httpResponse.statusCode).toBe(500)
