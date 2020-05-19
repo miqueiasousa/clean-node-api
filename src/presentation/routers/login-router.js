f4ae1751f126634f9bee0f03f6a9c48daab42081
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-05-19 20:48:38 -0300

    ensure LoginRouter returns 401 when invalid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,5 +1,7 @@
 const HttpResponse = require('../helpers/http-response')
 
+const makeHttpResponse = req => new HttpResponse(req)
+
 module.exports = class LoginRouter {
   constructor (authUseCase) {
     this.authUseCase = authUseCase
@@ -7,15 +9,17 @@ module.exports = class LoginRouter {
 
   route (httpRequest) {
     if (!httpRequest || !httpRequest.body) {
-      return new HttpResponse(500, 'Internal server error')
+      return makeHttpResponse(500, 'Internal server error')
     }
 
     const { email, password } = httpRequest.body
 
     if (!email || !password) {
-      return new HttpResponse(400, 'Bad request')
+      return makeHttpResponse(400, 'Bad request')
     }
 
     this.authUseCase.auth(email, password)
+
+    return makeHttpResponse(401, 'Unauthorized')
   }
 }
