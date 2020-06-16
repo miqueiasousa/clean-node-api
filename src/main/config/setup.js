d925dfc2555c1524ef73ba6a987f6800cb147abd
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-06-16 16:14:56 -0300

    Enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,9 @@
 module.exports = (app) => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    res.header('access-control-allow-origin', '*')
+    res.header('access-control-allow-methods', '*')
+    res.header('access-control-allow-headers', '*')
+    next()
+  })
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -1,12 +1,20 @@
 const req = require('supertest')
 const app = require('./app')
 
+app.get('/test')
+
 describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
-    app.get('/test')
-
     const res = await req(app).get('/test')
 
     expect(res.header['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    const res = await req(app).get('/test')
+
+    expect(res.header['access-control-allow-origin']).toBe('*')
+    expect(res.header['access-control-allow-methods']).toBe('*')
+    expect(res.header['access-control-allow-headers']).toBe('*')
   })
 })
